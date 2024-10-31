94ae833902276761a7b70893177d9efd43cf4cd4
rusted-dreams <123829783+rusted-dreams@users.noreply.github.com>
2024-10-31 23:00:47 +0530

    kuch to kiya hai

---
+++ src/app/contact-me/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const contactme = () => {
+  return (
+    <div>contact-me</div>
+  )
+}
+
+export default contactme
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <>
     <div>Welcome to my blogs</div>
+    <Button>click me</Button>
     </>
     
   );
