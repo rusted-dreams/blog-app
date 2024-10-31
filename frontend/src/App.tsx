a4a3a1c32afcf2050d0cb876403c8168cabfb267
rusted-dreams <123829783+rusted-dreams@users.noreply.github.com>
2024-10-31 12:20:16 +0530

    frontend setup - create vite app, add tailwind, add shadcnUi

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+
+// import "./App.css"
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      {/* <div className="bg-orange-300 text-slate-700 p-0">hello there</div> */}
+      <Button>font is lower</Button>
+    </>
+  )
+}
+
+export default App
