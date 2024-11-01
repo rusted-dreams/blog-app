b838eb00be5bf86ca5e90ec342e15cfb0d53b29b
rusted-dreams <123829783+rusted-dreams@users.noreply.github.com>
2024-11-01 17:37:43 +0530

    basic navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import { Button } from "@/components/ui/button";
-
+import HomePage from "@/components/custom/Home";
 export default function Home() {
   return (
     <>
-    <div>Welcome to my blogs</div>
-    <Button>click me</Button>
+    <HomePage />
     </>
     
   );
---
+++ src/components/custom/Navbar.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Button } from '../ui/button'
+import Link from 'next/link'
+
+const Navbar = () => {
+  return (
+    <>
+    <nav className='p-2 sticky top-0 backdrop-blur-xl'>
+      <div className='container flex border-b justify-between items-center mx-auto w-full rounded-md'>
+        <div className='text-lg font-bold'>SM</div>
+        <div className='flex items-center gap-2 mb-2'>
+          <Link href='/'>HOME</Link>
+          <Link href='/about'>About</Link>
+          <Link href='/contact'>Contact</Link>
+          <Button size='sm'>Post a Blog </Button>
+          
+        </div>
+        </div>
+    </nav>
+    </>
+  )
+}
+
+export default Navbar
